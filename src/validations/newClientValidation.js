3b4047967e5798a9a2a028bb8cb5e7c4f00a2282
Isaac Moreira Barbosa <izass1312@gmail.com>
2021-02-22 23:13:11 -0300

    Don't allow negative numbers on client number fields

--- src/validations/newClientValidation.js
+++ src/validations/newClientValidation.js
@@ -2,8 +2,12 @@ import * as yup from "yup";
 
 const schema = yup.object({
   value: yup.string().required("This field is required"),
-  monthyPrice: yup.number("Field must be a number").required("This field is required"),
-  setupPrice: yup.number("Field must be a number").required("This field is required"),
+  monthyPrice: yup.number("Field must be a number")
+    .positive("This fild must be positive")
+    .required("This field is required"),
+  setupPrice: yup.number("Field must be a number")
+    .positive("This fild must be positive")
+    .required("This field is required"),
   currency: yup.string().required("This field is required")
 });
 
